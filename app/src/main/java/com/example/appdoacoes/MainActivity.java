3e4050edbe554f794af5c99150f03aad1ce8c92a
skypse <skypse0136@gmail.com>
2025-05-05 17:58:40 -0300

    commit databasehelper funcionando

--- app/src/main/java/com/example/appdoacoes/MainActivity.java
+++ app/src/main/java/com/example/appdoacoes/MainActivity.java
@@ -1,21 +1,22 @@
 package com.example.appdoacoes;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.appdoacoes.data.DatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
-    DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        db = new DatabaseHelper(this);
+        // Instancia o banco de dados
+        DatabaseHelper dbHelper = new DatabaseHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
         Log.d("MainActivity", "Instância do banco criada.");
     }
 }
